03010bce04d182e5efdbc659aa64248dddd60bb6
sejhyun13 <sejhyun13@gmail.com>
2026-02-20 14:05:36 +0900

    2026 02 20 14:05

--- SSAFY_SWEA/07.Tree/이진 탐색/이진 탐색.py
+++ SSAFY_SWEA/07.Tree/이진 탐색/이진 탐색.py
@@ -3,21 +3,22 @@
 import sys;sys.stdin = open('이진탐색input.txt')
 
 
-def recur(nums, i): # i : 넣을 노드 번호
-    mid = max(nums) // 2 + 1
-    tree[i][2] = mid
-    # tree[i][0] =
-    # tree[i][1] =
-    left = nums[1:mid]
-    right = nums[mid+1:n+1]
-    if i != 0 and 2*i + 1 < len(nums):
-        recur(left, 2*i)
-        recur(right, 2*i + 1)
+def recur(node):
+    global num
+    if node > n:
+        return
+    recur(node * 2)
+    tree[node] = num
+    num += 1
+    recur(node * 2 + 1)
+    return
+# return의 기능은 '이전으로 복귀'임을 꼭 기억하자
+
 
 T = int(input())
 for tc in range(1,T+1):
     n = int(input()) # 정점 갯수
-    arr = list(range(n+1))
-    tree = [[0] * 3 for _ in range(n+1)]
-    recur(arr, 1)
-    print(tree)
+    tree = [0] * (n+1) # 완전 이진 트리 -> 1차원 배열
+    num = 1
+    recur(1)
+    print(f'#{tc} {tree[1]} {tree[n//2]}')
